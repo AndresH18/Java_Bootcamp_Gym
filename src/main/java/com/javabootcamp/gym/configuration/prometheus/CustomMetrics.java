f5a7b6c1660a4ef1e3d90e09fd0ba4d9f1ecfc11
Andres Hoyos <andres_hoyos@epam.com>
2023-11-14 23:01:30 -0500

    Added environment properties, Health checks, custom health checks and metrics

---
+++ src/main/java/com/javabootcamp/gym/configuration/prometheus/CustomMetrics.java
@@ -0,0 +1,25 @@
+package com.javabootcamp.gym.configuration.prometheus;
+
+import org.springframework.stereotype.Component;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomMetrics {
+    private final Counter customCounter;
+
+    public CustomMetrics(MeterRegistry meterRegistry) {
+        this.customCounter = Counter.builder("custom_counter")
+                .description("A custom counter metric")
+                .register(meterRegistry);
+    }
+
+    public void increment() {
+        customCounter.increment();
+    }
+
+    public double getCount() {
+        return customCounter.count();
+    }
+}
